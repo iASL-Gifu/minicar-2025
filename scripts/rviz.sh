184b9f99e8634d632cd0e4af6e57dbeddc03ac4c
peregrine-884 <hysh.hyt.0235@gmail.com>
2025-10-19 14:46:30 +0900

    feat: update rviz display configuration

--- scripts/rviz.sh
+++ scripts/rviz.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-rviz2 -d $(ros2 pkg prefix isaac_ros_visual_slam --share)/rviz/default.cfg.rviz
+# ===== 設定 =====
+# ワークスペースのパス（適宜変更）
+WS_PATH=/workspaces
+
+# ===== ROS 2 環境を読み込み =====
+source ${WS_PATH}/install/setup.bash
+
+# ===== rviz設定ファイルのパスを取得 =====
+RVIZ_PATH="$(ros2 pkg prefix localization_launch)/share/localization_launch/rviz/rviz.rviz"
+
+# ===== ファイル存在チェック =====
+if [ -f "$RVIZ_PATH" ]; then
+  echo "RViz設定ファイルが見つかりました: $RVIZ_PATH"
+  echo "RViz2を起動します..."
+  rviz2 -d "$RVIZ_PATH"
+else
+  echo "RViz設定ファイルが見つかりません: $RVIZ_PATH"
+  exit 1
+fi
